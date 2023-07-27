1bf77908c35b42b3130aaad3c6332c1cb835dc81
Kirill <kirunya1993@gmail.com>
2023-07-27 21:09:57 +0200

    Add audit-service, pg-admin and nginx

--- user-service/src/main/java/by/it_academy/user_service/endpoints/web/ControllerExceptionHandler.java
+++ user-service/src/main/java/by/it_academy/user_service/endpoints/web/ControllerExceptionHandler.java
@@ -17,7 +17,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.*;
