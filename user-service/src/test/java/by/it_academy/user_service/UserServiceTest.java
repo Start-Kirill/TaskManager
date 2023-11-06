9dda6506a77b72153ed5343ff7dd95516e066d87
Kirill <kirunya1993@gmail.com>
2023-11-06 18:34:49 +0100

    User service tests were added

--- user-service/src/test/java/by/it_academy/user_service/UserServiceTest.java
+++ user-service/src/test/java/by/it_academy/user_service/UserServiceTest.java
@@ -1,7 +1,10 @@
 package by.it_academy.user_service;
 
+import by.it_academy.task_manager_common.dto.CustomPage;
 import by.it_academy.task_manager_common.enums.UserRole;
 import by.it_academy.task_manager_common.enums.UserStatus;
+import by.it_academy.task_manager_common.exceptions.common.VersionsNotMatchException;
+import by.it_academy.task_manager_common.exceptions.structured.NotCorrectPageDataException;
 import by.it_academy.user_service.core.dto.UserCreateDto;
 import by.it_academy.user_service.dao.api.IUserDao;
 import by.it_academy.user_service.dao.entity.User;
@@ -9,6 +12,7 @@ import by.it_academy.user_service.service.UserHolder;
 import by.it_academy.user_service.service.UserService;
 import by.it_academy.user_service.service.api.IUserAuditService;
 import by.it_academy.user_service.service.exceptions.common.UserNotExistsException;
+import by.it_academy.user_service.service.exceptions.structured.MailNotExistsException;
 import by.it_academy.user_service.service.exceptions.structured.NotValidUserBodyException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,10 +24,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -99,6 +107,31 @@ public class UserServiceTest {
                     UUID uuid = m.getArgument(0, UUID.class);
                     return users.stream().filter(u -> u.getUuid().equals(uuid)).findFirst();
                 });
+
+        Mockito.lenient().when(userDao.findAll(Mockito.any(PageRequest.class)))
+                .thenAnswer(m -> new PageImpl<>(users));
+
+        Mockito.lenient().when(conversionService.convert(Mockito.any(Page.class), Mockito.eq(CustomPage.class)))
+                .thenAnswer(m -> {
+                    CustomPage<User> userCustomPage = new CustomPage<>();
+                    userCustomPage.setContent(m.getArgument(0, Page.class).getContent());
+                    return userCustomPage;
+                });
+        Mockito.lenient().when(this.userDao.existsByMail(Mockito.any(String.class)))
+                .thenAnswer(m -> {
+                    String mail = m.getArgument(0, String.class);
+                    return users.stream().anyMatch(u -> u.getMail().equals(mail));
+                });
+        Mockito.lenient().when(this.userDao.findByMail(Mockito.any(String.class)))
+                .thenAnswer(m -> {
+                    String mail = m.getArgument(0, String.class);
+                    return users.stream().filter(u -> u.getMail().equals(mail)).findFirst();
+                });
+        Mockito.lenient().when(this.userDao.findAllByUuidIn(Mockito.any(Collection.class)))
+                .thenAnswer(m -> {
+                    Collection<UUID> willingUsers = (Collection<UUID>) m.getArgument(0, Collection.class);
+                    return users.stream().filter(u -> willingUsers.contains(u.getUuid())).toList();
+                });
     }
 
     @Test
@@ -151,10 +184,99 @@ public class UserServiceTest {
         Assertions.assertThrows(UserNotExistsException.class, () -> this.userService.get(UUID.randomUUID()));
     }
 
-    //    TODO
+    @Test
+    public void shouldGetPage() {
+        Assertions.assertNotNull(userService.get(0, 1));
+    }
+
+    @Test
+    public void shouldThrowWhileGetPage() {
+        Assertions.assertThrows(NotCorrectPageDataException.class, () -> {
+            userService.get(0, 0);
+        });
+    }
+
+    @Test
+    public void shouldGetPageProperly() {
+        CustomPage<User> userCustomPage = this.userService.get(0, 1);
+        Assertions.assertEquals(userCustomPage.getContent(), users);
+        Assertions.assertEquals(userCustomPage.getSize(), 1);
+        Assertions.assertEquals(userCustomPage.getNumber(), 0);
+    }
+
+    @Test
+    public void shouldFindAllByUuids() {
+        List<UUID> willingUsers = List.of(FIRST_USER_UUID, SECOND_USER_UUID);
+        Assertions.assertNotNull(userService.findAllByUuid(willingUsers));
+    }
+
+    @Test
+    public void shouldGetEmptyWhileFindAllByUuids() {
+        List<UUID> willingUsers = List.of(UUID.randomUUID());
+        Assertions.assertTrue(userService.findAllByUuid(willingUsers).isEmpty());
+    }
+
+    @Test
+    public void shouldFindAllByUuidsProperly() {
+        List<UUID> willingUsers = List.of(FIRST_USER_UUID, SECOND_USER_UUID);
+        Assertions.assertEquals(users, userService.findAllByUuid(willingUsers));
+    }
+
+    @Test
+    public void shouldFindByMail() {
+        Assertions.assertNotNull(this.userService.findByMail(CORRECT_MAIL));
+    }
+
+    @Test
+    public void shouldThrowWhileFindByMail() {
+        Assertions.assertThrows(MailNotExistsException.class, () -> {
+            this.userService.findByMail(INCORRECT_MAIL);
+        });
+    }
+
+    @Test
+    public void shouldFindByMailProperly() {
+        Assertions.assertEquals(CORRECT_MAIL, this.userService.findByMail(CORRECT_MAIL).getMail());
+    }
+
+
     @Test
     public void shouldUpdate() {
+        UserCreateDto correctUserCreateDto = generateCorrectUserCreate();
+        LocalDateTime realVersion = this.users.get(0).getDateTimeUpdate();
+        Assertions.assertNotNull(this.userService.update(
+                correctUserCreateDto,
+                FIRST_USER_UUID,
+                realVersion));
+    }
 
+    @Test
+    public void shouldThrowWhileUpdate() {
+        UserCreateDto correctUserCreateDto = generateCorrectUserCreate();
+        LocalDateTime wrongVersion = LocalDateTime.now();
+        Assertions.assertThrows(VersionsNotMatchException.class, () -> {
+            this.userService.update(
+                    correctUserCreateDto,
+                    FIRST_USER_UUID,
+                    wrongVersion
+            );
+        });
+    }
+
+    @Test
+    public void shouldUpdateProperly() {
+        UserCreateDto userCreateDto = generateCorrectUserCreate();
+        LocalDateTime dateTimeUpdate = this.users.get(0).getDateTimeUpdate();
+        LocalDateTime dateTimeCreate = this.users.get(0).getDateTimeCreate();
+        User update = this.userService.update(userCreateDto, FIRST_USER_UUID, dateTimeUpdate);
+        Assertions.assertEquals(userCreateDto.getFio(), update.getFio());
+        Assertions.assertEquals(userCreateDto.getMail(), update.getMail());
+        Assertions.assertEquals(userCreateDto.getPassword(), update.getPassword());
+        Assertions.assertEquals(userCreateDto.getRole(), update.getRole());
+        Assertions.assertEquals(userCreateDto.getStatus(), update.getStatus());
+        Assertions.assertEquals(dateTimeCreate, update.getDateTimeCreate());
+        Assertions.assertEquals(dateTimeUpdate, update.getDateTimeUpdate());
+        Assertions.assertEquals(FIRST_USER_UUID, update.getUuid());
     }
 
 
@@ -177,6 +299,7 @@ public class UserServiceTest {
                 INCORRECT_PASSWORD
         );
     }
+
 
     private List<User> generateFakeDBData() {
         List<User> users = new ArrayList<>();
